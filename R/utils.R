77fa55e82b4696bcf298fbe4b2f723cf7b46e550
mshilts1 <meghanshilts@gmail.com>
2025-09-08 11:11:50 -0500

    current BioStore storage and capacity

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,16 @@
-nothing <- function(){
-  # nothing here
+#' Load in BioStore capacity Excel file from Karen Beeri
+#'
+#' @param x Exact name of the file
+#'
+#' @returns A clean tibble of the Excel file for further manipulation
+#' @export
+#' @importFrom readxl read_xlsx
+#' @importFrom janitor make_clean_names
+#'
+#' @examples
+#' readKBExcel()
+readKBExcel <- function(x = "calculator_for_Suman.xlsx"){
+  path <- system.file("extdata", x, package = "biostoreCapacity", mustWork = TRUE)
+  print(path)
+  readxl::read_xlsx(path = path, .name_repair = janitor::make_clean_names)
 }
